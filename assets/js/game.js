234cc86ab76d740079626b246068eee88e3f7da7
Joona Lusa <joona.lusa@hotmail.com>
2021-10-28 17:17:16 -0400

    added functionality to the fight function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,36 @@
-// this creates a function named "fight"
+// User attributes
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-    window.alert("The fight has begun!");
+// Enemy's attributes
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators");
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+
+    // Checking enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " had died!");
+        enemyHealth = 50;
+    } else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+    // Checking player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+        playerHealth = 100;
+    } else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
 };
 
-var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+fight();
