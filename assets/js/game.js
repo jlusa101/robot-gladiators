e35ac9a1ea2c57fa04c0b88da222671e891e65e4
Joona Lusa <joona.lusa@hotmail.com>
2021-10-30 08:17:12 -0400

    fixed name prompt bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,9 +4,16 @@ var randomNumber = function(min, max) {
 
     return value;
 };
+// Function to get the player name with validators
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+};
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
